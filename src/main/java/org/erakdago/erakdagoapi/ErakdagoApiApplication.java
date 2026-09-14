65fc9bdaee62f1ad8b467f895c74ac96f225ab28
fatratra-png <nyfatratrar@gmail.com>
2026-09-14 18:08:25 +0300

    Initial commit

---
+++ src/main/java/org/erakdago/erakdagoapi/ErakdagoApiApplication.java
@@ -0,0 +1,13 @@
+package org.erakdago.erakdagoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErakdagoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErakdagoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/erakdago/erakdagoapi/ErakdagoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erakdago.erakdagoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErakdagoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
