1976e93c4484ed341fa1308b3149995701eedd3b
fatratra-png <nyfatratrar@gmail.com>
2026-09-22 15:51:15 +0300

    refactor: rename EndemicSpecies to Species
    Co-authored-by: ItokianaRAKT <itokianarakt@gmail.com>

---
+++ src/main/java/org/erakdago/erakdagoapi/model/Species.java
@@ -0,0 +1,32 @@
+package org.erakdago.erakdagoapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Species {
+    private UUID id;
+    private String localname;
+    private String scientificName;
+    private String description;
+    private String habitat;
+    private ConservationStatus conservationStatus;
+    private List<String> images;
+    private boolean isEndemic;
+    private SpeciesType type;
+    private UUID towndID;
+    public enum ConservationStatus {
+        CR, EN, VU, NT, LC, DD, NE
+    }
+    public enum SpeciesType {
+            FAUNA,FLORA
+    }
+}
